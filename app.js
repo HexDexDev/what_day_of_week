a5fda3ead9f60ac7096aa345c6bc6acb70564743
HexDexDev <138143730+HexDexDev@users.noreply.github.com>
2024-06-19 15:50:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Prompt perguntando qual o dia da semana
+let qualDiaSemana = prompt("Qual é o dia da Semana");
+
+// Se o dia da semana for Sábado ou Domingo vai mostrar a mensagem Bom final de Semana
+if (qualDiaSemana == "Sábado" || qualDiaSemana == "Domingo") {
+alert("Bom Final de Semana");
+} 
+
+// Qualquer outra mensagem mostrará a mensagem boa semana
+else {
+ alert("Boa Semana");
+}
+
